15269d598adb88ac2cb07266237772d0ab9be385
Patricio <gpatricio@ua.pt>
2017-02-17 21:18:38 +0000

    Pop up

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -12,3 +12,14 @@ function initMap() {
         map: map
     });
 }
+var dialog = document.querySelector('dialog');
+    var showDialogButton = document.querySelector('#show-dialog');
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    showDialogButton.addEventListener('click', function() {
+      dialog.showModal();
+    });
+    dialog.querySelector('.close').addEventListener('click', function() {
+      dialog.close();
+    });
