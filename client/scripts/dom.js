e17abd6ee36f5e868b88fc78d9af92cd5aacf9a4
Luís Silva <lfssilva@ua.pt>
2017-02-18 22:25:55 +0000

    Refactor DOM

--- client/scripts/dom.js
+++ client/scripts/dom.js
@@ -1,17 +1,33 @@
 function logged_out() {
     var btn = document.getElementById('button_login');
-    btn.style.display = 'block';
+    var mapElement = document.getElementById('map');
+    var info = document.getElementById('info');
+    var info_lobby = document.getElementById('info_lobby');
+    var info_account = document.getElementById('info_account');
+    var fab = document.getElementById('create-lobby-btn');
+    var self_lobby = document.getElementById('selflobby');
 
-    var map = document.getElementById('map');
-    map.style.display = 'none';
-
-    var fab = document.getElementById('show-dialog');
-    fab.style.display = 'none';
-
-    var obfuscator = document.querySelector('.mdl-layout__obfuscator');
-    obfuscator.classList.remove('is-visible');
-
-    var drawer = document.querySelector('.mdl-layout__drawer');
-    drawer.classList.remove('is-visible');
+    mapElement.style.display = 'none';
+    btn.style.display = "block";
+    info.style.display = "block";
+    info_lobby.style.display = "block";
+    self_lobby.style.display = "none";
+    fab.style.display = "none";
 }
 
+function logged_in(){
+    var btn = document.getElementById('button_login');
+    var mapElement = document.getElementById('map');
+    var info = document.getElementById('info');
+    var info_lobby = document.getElementById('info_lobby');
+    var info_account = document.getElementById('info_account');
+    var fab = document.getElementById('create-lobby-btn');
+    var self_lobby = document.getElementById('selflobby');
+    mapDraw();
+    mapElement.style.display = 'block';
+    btn.style.display = "none";
+    info.style.display = "none";
+    info_lobby.style.display = "none";
+    fab.style.display = "block";
+    self_lobby.style.display = "block";
+}
